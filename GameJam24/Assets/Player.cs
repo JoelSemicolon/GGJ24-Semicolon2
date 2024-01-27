27841bcac4c916bda81dcb682ee4f7d86d7aa4f8
Big-Jeans <143380936+Big-Jeans@users.noreply.github.com>
2024-01-27 15:15:10 +0800

    Update Player.cs

--- GameJam24/Assets/Player.cs
+++ GameJam24/Assets/Player.cs
@@ -7,7 +7,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     Rigidbody rigidbody;
 
-    float movespeed = 5f;
+    float moveSpeed = 5f;
+
+    bool highJump = false;
+    bool sprint = false;
+    bool breakObjects = false;
+    bool glide = false;
 
     void Start()
     {
@@ -17,21 +22,66 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (highJump)
+            {
+                highJump = false;
+            }
+            else
+            {
+                highJump = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (sprint)
+            {
+                sprint = false;
+            }
+            else
+            {
+                sprint = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (breakObjects)
+            {
+                breakObjects = false;
+            }
+            else
+            {
+                breakObjects = true;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (glide)
+            {
+                glide = false;
+            }
+            else
+            {
+                glide = true;
+            }
+        }
+
         if (Input.GetKey(KeyCode.W) && !Physics.BoxCast(transform.position, new Vector3(0.49f, 0.99f, 0.49f), Vector3.forward, Quaternion.identity, 0.1f))
         {
-            rigidbody.MovePosition(rigidbody.position + transform.forward * movespeed * Time.deltaTime);
+            rigidbody.MovePosition(rigidbody.position + transform.forward * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A) && !Physics.BoxCast(transform.position, new Vector3(0.49f, 0.99f, 0.49f), Vector3.left, Quaternion.identity, 0.1f))
         {
-            rigidbody.MovePosition(rigidbody.position - transform.right * movespeed * Time.deltaTime);
+            rigidbody.MovePosition(rigidbody.position - transform.right * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S) && !Physics.BoxCast(transform.position, new Vector3(0.49f, 0.99f, 0.49f), Vector3.back, Quaternion.identity, 0.1f))
         {
-            rigidbody.MovePosition(rigidbody.position - transform.forward * movespeed * Time.deltaTime);
+            rigidbody.MovePosition(rigidbody.position - transform.forward * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D) && !Physics.BoxCast(transform.position, new Vector3(0.49f, 0.99f, 0.49f), Vector3.right, Quaternion.identity, 0.1f))
         {
-            rigidbody.MovePosition(rigidbody.position + transform.right * movespeed * Time.deltaTime);
+            rigidbody.MovePosition(rigidbody.position + transform.right * moveSpeed * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.Space) && Physics.BoxCast(transform.position, new Vector3(0.49f, 0.99f, 0.49f), Vector3.down, Quaternion.identity, 0.1f))
         {
