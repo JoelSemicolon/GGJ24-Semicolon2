faee85025929db39ba50064ab06c7c0dda326f5c
Big-Jeans <143380936+Big-Jeans@users.noreply.github.com>
2024-01-27 15:42:20 +0800

    Update Player.cs

--- GameJam24/Assets/Player.cs
+++ GameJam24/Assets/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     Rigidbody rigidbody;
 
     float moveSpeed = 5f;
+    float jumpHeight = 10f;
 
     bool highJump = false;
     bool sprint = false;
@@ -22,32 +23,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        Vector3 forward = Camera.main.transform.forward;
-        forward.y = 0f;
-        if (forward.sqrMagnitude > 0f)
-        {
-            forward.Normalize();
-        }
-        Vector3 right = Camera.main.transform.right;
-        right.y = 0f;
-        if (right.sqrMagnitude > 0f)
-        {
-            right.Normalize();
-        }
-
-        Vector3 moveDirection = Vector3.zero;
-
-        float sideMove = Input.GetAxisRaw("Horizontal");
-        float forwardMove = Input.GetAxisRaw("Vertical");
-
-        moveDirection += forward * forwardMove;
-        moveDirection += right * sideMove;
-
-        rigidbody.MovePosition(rigidbody.position + moveDirection * Time.deltaTime);
-
-        transform.rotation = Quaternion.LookRotation(forward);
-
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             if (highJump)
@@ -91,6 +66,37 @@ public class Player : MonoBehaviour
             {
                 glide = true;
             }
+        }
+
+        Vector3 forward = Camera.main.transform.forward;
+        forward.y = 0f;
+        if (forward.sqrMagnitude > 0f)
+        {
+            forward.Normalize();
+        }
+        Vector3 right = Camera.main.transform.right;
+        right.y = 0f;
+        if (right.sqrMagnitude > 0f)
+        {
+            right.Normalize();
+        }
+
+        Vector3 moveDirection = Vector3.zero;
+
+        float sideMove = Input.GetAxisRaw("Horizontal");
+        float forwardMove = Input.GetAxisRaw("Vertical");
+
+        moveDirection += forward * forwardMove;
+        moveDirection += right * sideMove;
+
+        rigidbody.MovePosition(rigidbody.position + moveDirection * Time.deltaTime);
+
+        transform.rotation = Quaternion.LookRotation(forward);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.BoxCast(transform.position, new Vector3(0.49f, 0.99f, 0.49f), Vector3.down, Quaternion.identity, 0.1f))
+        {
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.VelocityChange);
         }
     }
 }
